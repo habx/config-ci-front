f1e480463e4c2bc3693e120beff49d5feb6318d1
CarolineVP <caroline_pujebet@hotmail.com>
2022-07-07 09:59:38 +0200

    Add `loadash` alias

--- vite/client.mjs
+++ vite/client.mjs
@@ -99,6 +99,7 @@ export default defineConfig(async (params) => {
         '@' : resolve(cwd(), 'src'),
         'querystring': 'qs',
         'polygon-clipping': 'polygon-clipping/dist/polygon-clipping.cjs.js',
+        'loadash': 'lodash-es'
       },
       dedupe,
     },
