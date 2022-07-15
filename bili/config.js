64e613974a3de05521abc6bf843e9a30a8db65f9
Alexis Fontaine <alexis.fontaine@outlook.com>
2022-07-15 23:44:29 +0200

    Remove useless dependencies

--- bili/config.js
+++ bili/config.js
@@ -1,12 +1,6 @@
-const helpers = require('../helpers')
-
 /** @type {import('bili').Config} */
 exports.config = {
   plugins: {
-    analyzer: helpers.exists(
-      'rollup-plugin-analyzer',
-      'install the package to enable bundle size analysis.'
-    ),
     babel: false,
     typescript2: {
       useTsconfigDeclarationDir: true,
